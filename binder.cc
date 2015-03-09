a9316f4bf1786e71802dd7f986e79bcd573cbcab
Terry sun <terrysun88@gmail.com>
2015-03-09 03:10:48 -0400

    modified binder main socket connect function

--- binder.cc
+++ binder.cc
@@ -12,6 +12,7 @@
 #include <sys/ioctl.h>
 #include <vector>
 #include <map>
+#include <sys/utsname.h>
 
 
 using namespace std;
@@ -44,7 +45,18 @@ void error(string s) {
     exit(1);
 }
 
-
+/*
+pair<string, int*> service_search(pair<string, int*> key) {
+    map <pair<string, int*>, vector<struct serverInfo> >::iterator it;
+    for (it = procedure_db.begin(); it != procedure_db.end(); ++it) {
+        pair<string, int*> tmp = it->first;
+        if (sizeof(key.second) == sizeof(tmp.second)) {
+            
+        }
+    }
+    
+}
+*/
 
 int addServerService(string hostname, int server_port, string funcName, int* args) {
     // creating key which is formed by  function name and args type
@@ -54,6 +66,17 @@ int addServerService(string hostname, int server_port, string funcName, int* arg
     struct serverInfo location;
     location.create_info(hostname, server_port);
     
+    // no any server exist for the request service
+    if (procedure_db.find(key) == procedure_db.end()) {
+        procedure_db[key].push_back(location);
+    }
+    
+    else {
+        vector<struct serverInfo> service_list;
+        service_list = procedure_db[key];
+        
+        
+    }
     
     return 0;
 }
@@ -62,7 +85,7 @@ void server_register(int len, int fd) {
 
     bool resigter_success = true;
     char msg_buff[len]; // receive msg from server request
-    memset(&msg_buff, 0, sizeof(msg_buff));
+    memset(msg_buff, 0, sizeof(msg_buff));
 
     int res = (int) recv(fd, msg_buff, sizeof(msg_buff), 0);
     if (res < 0) {
@@ -70,16 +93,16 @@ void server_register(int len, int fd) {
         cerr << "FAILED to receive msg from server request" << endl;
     }
     
-    string hostname, funcName;
+    char hostname[40], funcName[20];
     int server_port, offset = 0;
     // get hostname from msg
-    memcpy(&hostname, msg_buff, 40);
+    memcpy(hostname, msg_buff, 40);
     offset += 40;
     // get port number from msg
     memcpy(&server_port, &msg_buff[offset], 4);
     offset += 4;
     // get function name from msg
-    memcpy(&funcName, &msg_buff[offset], 20);
+    memcpy(funcName, &msg_buff[offset], 20);
     offset += 20;
     
     // compute the number of arguments
@@ -112,111 +135,106 @@ void terminateRequest() {
     
 }
 
-void handleAllRequest(int fd) {
+int handleAllRequest(int fd) {
+    int res = 0;
     char buff[22];
-    memset(&buff, 0, sizeof(buff));
-    int res = (int)recv(fd, buff, sizeof(buff), 0);
+    res = recv(fd, buff, 22, 0);
     if (res < 0) {
         error("FAILED to receive len & MSG_TYPE");
     }
     
     int len;
-    string MSG_TYPE;
+    char MSG_TYPE[18];
     memcpy(&len, buff, 4);
+    cout << len << endl;
     len -= 22; // msg size that include hostname, portNum, funcName and argsType
-    memcpy(&MSG_TYPE, &buff[4], 18);
-    
-    if (MSG_TYPE == "REGISTER")
+    memcpy(MSG_TYPE, &buff[4], 18);
+    cout << MSG_TYPE << endl;
+    if (strcmp(MSG_TYPE, "REGISTER") == 0)
         server_register(len, fd);
-    else if (MSG_TYPE == "LOC_REQUEST")
+    else if (strcmp(MSG_TYPE, "LOC_REQUEST") == 0)
         handleLocRequest();
-    else if (MSG_TYPE == "")
-        error(""); //need to modified
-    else if (MSG_TYPE == "TERMINATE")
+    else if (strcmp(MSG_TYPE, "TERMINATE") == 0)
         terminateRequest();
     else {
         error("receiving incorrect request!");
     }
+    
+    return 0;
 }
 
 int main() {
-    int localSocket, port, res;
-    struct sockaddr_in binder_addr, cur_addr;
-    char binder_address[256];
-    fd_set workfds, readfds;
-    
-    localSocket = socket(AF_INET, SOCK_STREAM, 0);
-    if (localSocket < 0) {
-        error("Opening socket Failed!");
-    }
-    
-    bzero(&binder_addr, sizeof(binder_addr));
-    
-    binder_addr.sin_family = AF_INET;
-    binder_addr.sin_addr.s_addr = INADDR_ANY;
-    binder_addr.sin_port = 0;
-    
-    if (bind(localSocket, (struct sockaddr *)&binder_addr, sizeof(binder_addr)) < 0) {
-        error("Failed to bind server");
-    }
 
-    socklen_t port_len = sizeof(cur_addr);
-    
-    if (getsockname(localSocket, (struct sockaddr *)&cur_addr, &port_len) < 0) {
-        error("Failed to get server address");
+
+    //char *addr= getenv("SERVER_ADDRESS");
+    int status;
+    int s,new_sfd;
+    fd_set active_set, read_set;
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size;
+    struct addrinfo hints;
+    struct addrinfo *servinfo; // will point to the results
+    memset(&hints, 0, sizeof hints); // make sure the struct is empty
+    hints.ai_family = AF_UNSPEC; // don't care IPv4 or IPv6
+    hints.ai_socktype = SOCK_STREAM; // TCP stream sockets
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+    if ((status = getaddrinfo(INADDR_ANY, "0", &hints, &servinfo)) != 0) {
+        fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+        exit(1);
     }
-    
-    gethostname(binder_address, 1023);
-    port = ntohs(cur_addr.sin_port);
-    printf("BINDER_ADDRESS %s\n", binder_address);
-    printf("BINDER_PORT %d\n", port);
-    
-    listen(localSocket, 5);
-    
-    int fd_size = localSocket;
-    FD_ZERO(&readfds);
-    FD_SET(localSocket, &readfds);
-    
-    while (!terminal_signal) {
-        int nread;
-        memcpy(&workfds, &readfds, sizeof(readfds));
-        
-        res = select(fd_size + 1, &workfds, NULL, NULL, NULL);
-        if (res < 0) {
-            error("selection Failed");
+    //create socket and bind to a available port
+    s = socket(servinfo->ai_family, servinfo->ai_socktype, servinfo->ai_protocol);
+    status = bind(s, servinfo->ai_addr, servinfo->ai_addrlen);
+    //print hostname and port number
+    struct utsname name;
+    uname(&name);
+    const char* hostname=name.nodename;
+    //printf("SERVER_ADDRESS %s\n",hostname);
+    struct sockaddr_in sin;
+    socklen_t leng = sizeof(sin);
+    struct hostent* retval=gethostbyname(hostname);
+    printf("BINDER_ADDRESS %s\n",retval->h_name);
+    if (getsockname(s, (struct sockaddr *)&sin, &leng) == -1)
+        perror("getsockname");
+    else
+        printf("BINDER_PORT %d\n", ntohs(sin.sin_port));
+    //max 5 clients
+    listen(s,5);
+    freeaddrinfo(servinfo);
+    //init select()
+    FD_ZERO(&active_set);
+    FD_SET(s, &active_set);
+    for (;;) {
+        //assign backup to ready queue
+        read_set = active_set;
+        //modify read_set drop socket without new message
+        status = select (FD_SETSIZE, &read_set, NULL, NULL, NULL);
+        if (status < 0) {
+            perror ("select() failed");
+            exit(1);
         }
-        
-        for (int fd = 0; fd < fd_size; fd++) {
-            if (FD_ISSET(localSocket, &workfds)) {
-                if (fd == localSocket) {
-                    socklen_t client_size;
-                    int connectionSocket;
-                    struct sockaddr_in client_addr;
-                    client_size = sizeof(client_addr);
-                    connectionSocket = accept(localSocket, (struct sockaddr *)&client_addr, &client_size);
-                    if (connectionSocket < 0) {
-                        error("Accepting request failed");
+        for (int i = 0; i < FD_SETSIZE; i++)
+            if (FD_ISSET(i, &read_set)) {
+                if (i == s) {
+                    // New Connection
+                    addr_size = sizeof their_addr;
+                    new_sfd = accept(s, (struct sockaddr *)&their_addr, &addr_size);
+                    if (new_sfd < 0) {
+                        perror("accept() failed");
+                        exit(1);
                     }
-                    
-                    FD_SET(connectionSocket, &readfds);
-                    fd_size += 1;
-                }
-                else {
-                    ioctl(nread, FIONREAD, &readfds);
-                    if (nread == 0) {
-                        close(fd);
-                        FD_CLR(fd, &readfds);
-                    }
-                    else {
-                        handleAllRequest(fd);
+                    FD_SET(new_sfd, &active_set);
+                } else {
+                    // Data arriving on connected socket.
+                    status = handleAllRequest(i);
+                    if (status < 0) {
+                        close(i);
+                        FD_CLR(i, &active_set);
                     }
                 }
             }
-        }
-        
     }
-    
-    close(localSocket);
+
     exit(0);
 }
 
