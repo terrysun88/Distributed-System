3bab21612893cf02319d0f6e1088e066d63c6ea0
baizhenyu <tim.baizhenyu@gmail.com>
2015-03-08 21:06:15 -0400

    add definition to rpcCALL, now it can connects to binder and retrive server info

--- rpc.cc
+++ rpc.cc
@@ -96,8 +96,8 @@ int rpcRegister(char* name, int* argTypes, skeleton f) {
    string tmp;
    int status,s,length;
    s=server_binder_s;
-   char type[]="REGISTER";
    //prepare the message
+   char type[]="REGISTER";
    int i =0;
    while (argTypes[i] != NULL) i++;
    i++;
@@ -146,9 +146,11 @@ int rpcExecute() {
 
 int rpcCall(char* name, int* argTypes, void** args) {
 //open a connection to the binder
-   int status,s;
+   int status,s,byte_sent;
    char *addr= getenv("BINDER_ADDRESS");
    char *port= getenv("BINDER_PORT");
+   char server_addr[ADDRESS_LEN];
+   int server_port;
    struct addrinfo hints;
    struct addrinfo *servinfo; // will point to the results
    memset(&hints, 0, sizeof hints); // make sure the struct is empty
@@ -165,9 +167,53 @@ int rpcCall(char* name, int* argTypes, void** args) {
       cout << "connection to Binder failed: " << errno << endl;
       return -2; //for now, need to change later
    }
-   
-   
-
+//send LOC_REQUEST to binder
+   //prepare the message
+   char type[]="LOC_REQUEST";
+   int i =0;
+   while (argTypes[i] != NULL) i++;
+   i++;
+   //Assume length: type= 18; server address=40;port=4;name=20
+   int length=4+TYPE_LEN+NAME_LEN+i*sizeof(int);
+   //compose the message
+   /*Message format: 
+      length: int
+      type: char[]
+      name: char[]
+      argTypes: int[]
+   */
+   int offset = 0 ;
+   char msg[length];
+   memset(msg,0,sizeof(msg));
+   memcpy(&msg[offset],&length,4);
+   offset+=4;
+   memcpy(&msg[offset],type,TYPE_LEN);
+   offset+=TYPE_LEN;
+   memcpy(&msg[offset],name,NAME_LEN);
+   offset+=NAME_LEN;
+   memcpy(&msg[offset],argTypes,i*sizeof(int));
+   offset+=i*sizeof(int);
+   //send the message to binder
+   int bytes_sent = send(s, msg, length, 0);
+   if (bytes_sent == -1) {
+      cout << "Client to Binder Failed: Unable to send" << endl;
+      return -2; //for now, need to change later
+   } 
+   //check the repley from binder
+   length = 4+TYPE_LEN+ADDRESS_LEN+PORT_LEN;
+   char buf[length];
+   status = recv(s, buf, length, 0);
+   char temp[TYPE_LEN];
+   memcpy(temp,&buf[4],TYPE_LEN);
+   if (strcmp(temp,"LOC_SUCCESS") == 0) {
+      memcpy(server_addr,&buf[4+TYPE_LEN],ADDRESS_LEN);
+      memcpy(&server_port,&buf[4+TYPE_LEN+ADDRESS_LEN],4);
+   } else {
+      int errorcode;
+      memcpy(&errorcode,&buf[4+TYPE_LEN],4);
+      return errorcode; //for now, need to change later
+   }
+//connect to the server
 }
 
 int rpcTerminate() {
