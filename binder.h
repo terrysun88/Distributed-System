fd7bb0b03b367e0ec4eee9793179590a327dc533
Terry sun <terrysun88@gmail.com>
2015-03-13 17:03:47 -0400

    adding binder.h file

--- binder.h
+++ binder.h
@@ -1,13 +1,38 @@
 //
 //  binder.h
-//  cs454-a3
 //
 //  Created by Terry sun on 2015-03-13.
 //  Copyright (c) 2015 Terry sun. All rights reserved.
 //
 
-#ifndef cs454_a3_binder_h
-#define cs454_a3_binder_h
+#ifndef __BINDER_H__
+#define __BINDER_H__
+
+bool terminal_signal = false;
+int register_error_no = 0;
+int reasonCode = 0;
+fd_set *sets;
+
+// database of service and server
+std::map <std::pair<std::string, std::vector<int> >, std::vector<struct serverInfo> > procedure_db;
+std::vector<struct serverInfo> roundRobin_list;
+
+
+std::pair<std::string, std::vector<int> > create_key(std::string name, std::vector<int> args);
+void error(std::string s);
+std::pair<std::string, std::vector<int> > service_search(std::pair<std::string, std::vector<int> > key);
+bool server_check(std::vector<struct serverInfo> service_list, struct serverInfo loc2);
+struct serverInfo loc_search(std::vector<struct serverInfo> locs);
+int addServerService(std::string hostname, int server_port, std::string funcName, std::vector<int> args, int fd);
+void clean_service(struct serverInfo loc);
+void remove_crash_server();
+
+
+//handle different type request
+int handleAllRequest(int fd);
+void server_register(int len, int fd);
+void handleLocRequest(int len, int fd);
+void terminateRequest();
 
 
 
