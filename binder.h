fe58e51e5bf34e0b96c00027271104d52e7ce2c6
Terry sun <terrysun88@gmail.com>
2015-03-13 14:59:26 -0400

    create binder.h, move struct and function to header file

---
+++ binder.h
@@ -0,0 +1,14 @@
+//
+//  binder.h
+//  cs454-a3
+//
+//  Created by Terry sun on 2015-03-13.
+//  Copyright (c) 2015 Terry sun. All rights reserved.
+//
+
+#ifndef cs454_a3_binder_h
+#define cs454_a3_binder_h
+
+
+
+#endif
